effb33830a1190eba0a5d6718e979e4f109cec2d
henry <jaohaohsuan@gmail.com>
2017-01-06 23:18:49 +0800

    bugfix of retage

--- retag.sh
+++ retag.sh
@@ -2,7 +2,7 @@
 set -e
 dir=`mktemp -d -u`
 bootkube render --asset-dir=$dir --api-servers=https://127.0.0.1:443 --etcd-servers=http://127.0.0.1:2379 --self-host-kubelet &> /dev/null
-cp -r /manifests/* $dir/manifests
+cp -r /manifests/* $dir/manifests/
 for f in `grep -rw $dir/manifests --include=\*.{yaml,yml,json,list} -e "image:" | awk -F ':' '{print $1}' | uniq`
   do
   for i in `sed -n 's/image: \([a-z]\+\)/\1/p' $f | tr -d ' ' | uniq`
